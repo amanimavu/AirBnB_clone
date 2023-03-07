3b6db41e2eab33ce0be448201d55774af6495556
Amani Mavu <amanimavu@gmail.com>
2023-03-07 20:14:25 +0300

    make code compliant to pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ from models.base_model import BaseModel
 
 import unittest
 
+
 class TestBaseModel(unittest.TestCase):
     """
 
@@ -16,12 +17,14 @@ class TestBaseModel(unittest.TestCase):
 
     def test_str(self):
         string = "[BaseModel]", \
-        "(fbe5f327-c8ec-4665-a162-c685bdef3b4a)",\
-        "{'id': 'fbe5f327-c8ec-4665-a162-c685bdef3b4a', 'created_at':",\
+            "(fbe5f327-c8ec-4665-a162-c685bdef3b4a)",\
+            "{'id': 'fbe5f327-c8ec-4665-a162-c685bdef3b4a', 'created_at':",\
             "datetime.datetime(2023, 3, 6, 15, 36, 14, 361696),",\
-            "'updated_at': datetime.datetime(2023, 3, 6, 15, 36, 14, 361696),",\
+            "'updated_at':",\
+            "datetime.datetime(2023, 3, 6, 15, 36, 14, 361696),",\
             "'name': 'My_First_Model', 'my_number': 89}"
         self.assertEqual(str(self.base), string)
+
 
 if __name__ == "__main__":
     unittest.main()
