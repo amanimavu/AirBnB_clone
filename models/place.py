0b04e504c275475189f6aa15af2acd8c80af272b
Amani Mavu <amanimavu@gmail.com>
2023-03-13 16:06:48 +0300

    ensure correct attributes are available for model place

--- models/place.py
+++ models/place.py
@@ -14,11 +14,12 @@ class Place(BaseModel):
     description, number_rooms, max_guest, latitude,
     longitude, price_by_night and amenity_ids.
     """
-    city = ""
+    city_id = ""
     user_id = ""
     name = ""
     description = ""
     number_rooms = 0
+    number_of_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
