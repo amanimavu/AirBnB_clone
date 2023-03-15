0279f4571925b9687a0897b805c347fc156e5e57
Amani Mavu <amanimavu@gmail.com>
2023-03-15 12:44:08 +0300

    make the test for save more detailed

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,9 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 import uuid
+from models.engine.file_storage import FileStorage
+import os
+import json
 
 
 class TestBaseModel(unittest.TestCase):
@@ -15,6 +18,18 @@ class TestBaseModel(unittest.TestCase):
     methods found in the BaseModel class
     """
     def setUp(self):
+        try:
+            self.file_path = FileStorage._FileStorage__file_path
+        except:
+            pass
+        try:
+            os.remove(self.file_path)
+        except:
+            pass
+        try:
+            FileStorage._FileStorage__objects.clear()
+        except:
+            pass
         self.base_1 = BaseModel()
 
     def test_init(self):
@@ -32,7 +47,19 @@ class TestBaseModel(unittest.TestCase):
         This test validates that the datetime for creation
         and update are different when the object is saved
         """
+        self.assertFalse(os.path.exists(self.file_path))
         self.base_1.save()
+        self.assertTrue(os.path.exists(self.file_path))
+        try:
+            FileStorage._FileStorage__objects.clear()
+        except:
+            pass
+        storage = FileStorage()
+        storage.reload()
+        obj_reloaded = list(storage.all().values())[0]
+        self.assertEqual(self.base_1.id,obj_reloaded.id)
+        self.assertEqual(self.base_1.created_at, obj_reloaded.created_at)
+        self.assertEqual(self.base_1.updated_at, obj_reloaded.updated_at)
         self.assertNotEqual(self.base_1.created_at, self.base_1.updated_at)
 
     def test_to_dict(self):
